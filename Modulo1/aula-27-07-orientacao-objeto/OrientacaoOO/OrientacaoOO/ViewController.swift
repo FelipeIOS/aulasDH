8740de42958fd56dbeb039b7d99b49d3eeee055f
felipe silva <felipe.instrutor.ios@gmail.com>
2020-07-27 21:46:11 -0300

    implementei a classe Animal e a Struct Animal2

--- Modulo1/aula-27-07-orientacao-objeto/OrientacaoOO/OrientacaoOO/ViewController.swift
+++ Modulo1/aula-27-07-orientacao-objeto/OrientacaoOO/OrientacaoOO/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var animal: Animal = Animal(tamanho: "Grande", raca: "Golden", cor: "Branco", qtdPatas: 4, genero: "Macho", especie: "Cachorro", idade: 4)
+        
+        print(animal.raca)
+        
+        var animal2: Animal2 = Animal2(tamanho: "pequeno", raca: "pitBul", cor: "cinza", qtdPatas: 4, genero: "macho", especie: "Cachorro", idade: 4)
+        
+
     }
-
-
 }
 
