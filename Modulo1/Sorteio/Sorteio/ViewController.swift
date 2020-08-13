d9afbbc68f57836afec19cb1b40ff53a276bd303
felipe silva <felipe.instrutor.ios@gmail.com>
2020-08-13 20:15:03 -0300

    update code

---
+++ Modulo1/Sorteio/Sorteio/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Sorteio
+//
+//  Created by Felipe Miranda on 13/08/20.
+//  Copyright © 2020 Digital House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+//MARK: GRUPO 1
+    
+    @IBOutlet weak var name1G1Label: UILabel!
+    @IBOutlet weak var name2g1Label: UILabel!
+    @IBOutlet weak var name3g1Label: UILabel!
+    @IBOutlet weak var name4g1Label: UILabel!
+    @IBOutlet weak var name5g1Label: UILabel!
+    
+//MARK: GRUPO 2
+    
+    @IBOutlet weak var name1G2Label: UILabel!
+    @IBOutlet weak var name2g2Label: UILabel!
+    @IBOutlet weak var name3g2Label: UILabel!
+    @IBOutlet weak var name4g2Label: UILabel!
+    @IBOutlet weak var name5g2Label: UILabel!
+    
+//MARK: GRUPO 3
+    
+    @IBOutlet weak var name1G3Label: UILabel!
+    @IBOutlet weak var name2g3Label: UILabel!
+    @IBOutlet weak var name3g3Label: UILabel!
+    @IBOutlet weak var name4g3Label: UILabel!
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tappedSortButton(_ sender: UIButton) {
+    
+    }
+    
+}
+
