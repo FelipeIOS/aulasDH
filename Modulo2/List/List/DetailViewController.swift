cc253b31aab4609f7879deb4122371ffc86fcd4b
felipe silva <felipe.instrutor.ios@gmail.com>
2020-08-20 19:03:06 -0300

    implementei as duas primeiras viewCOntrollers

---
+++ Modulo2/List/List/DetailViewController.swift
@@ -0,0 +1,78 @@
+//
+//  DetailViewController.swift
+//  List
+//
+//  Created by Felipe Miranda on 18/08/20.
+//  Copyright © 2020 Digital House. All rights reserved.
+//
+
+import UIKit
+
+
+//class Aluno {
+//    var nome: String = ""
+//}
+//
+//struct AlunoColegial {
+//    var nome: String = ""
+//}
+
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var produtoLabel: UILabel!
+    @IBOutlet weak var produto2Label: UILabel!
+    
+    var values: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+//        var aluno1: Aluno = Aluno()
+//        aluno1.nome = "Felipe"
+//        
+//        var aluno2: Aluno = Aluno()
+//        aluno2 = aluno1
+//        
+//        aluno2.nome = "Dog"
+//        print(aluno1.nome)
+
+        self.produtoLabel.text = values[0]
+        self.produto2Label.text = values[1]
+        print("DetailViewController-viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clicouFechar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("DetailViewController-viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("DetailViewController-viewDidAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("DetailViewController-viewDidDisappear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("DetailViewController-viewWillDisappear")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
