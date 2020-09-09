46f5b8070ff8dbe02bafefbe5d8d34f2e7fe159f
felipe silva <felipe.instrutor.ios@gmail.com>
2020-09-08 22:06:09 -0300

    update code

---
+++ Modulo2/CollectionView/CollectionView/CarrosCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  CarrosCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Felipe Miranda on 08/09/20.
+//  Copyright © 2020 Digital House. All rights reserved.
+//
+
+import UIKit
+
+class CarrosCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var modeloLabel: UILabel!
+    
+    @IBOutlet weak var anoLabel: UILabel!
+    
+    @IBOutlet weak var carroImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    func setupCell(carro: Carro) {
+        
+        self.modeloLabel.text = carro.modelo
+        self.anoLabel.text = carro.ano
+        self.carroImageView.image = UIImage(named: carro.image ?? "")
+ 
+    }
+
+}
